79425c60584a5d09e017fef0bba44c48d173e7a0
kaushiksaraf91 <kaushiksaraf91@gmail.com>
2023-06-24 15:46:45 +0530

    authO authentication to frontend done

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,28 +1,68 @@
 import "./navbar.css";
 import { Link } from "react-router-dom";
-
+import { useAuth0 } from "@auth0/auth0-react";
 
 function NavBar() {
+  const { loginWithRedirect } = useAuth0();
+  const { logout } = useAuth0();
+  const { user, isAuthenticated, isLoading } = useAuth0();
   return (
-
-    <nav className="navbar navbar-expand-lg navbar-light bg-light nav">
-  <Link to="/" className="navbar-brand" >Yatraverse</Link>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <nav className=" navbar-expand-lg navbar-light bg-light  header">
+      <Link to="/" className="navbar-brand">
+        Yatraverse
+      </Link>
+      {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
-  </button>
+  </button> */}
 
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav mr-auto">
-    <li className="nav-item active">
-        <Link to="/explore" className="nav-link" >Explore<span className="sr-only">(current)</span></Link>
-      </li>
-      <li className="nav-item active">
-        <Link to="/about" className="nav-link" >About Us<span className="sr-only">(current)</span></Link>
-      </li>
-      <li className="nav-item active">
-        <Link to="/contact" className="nav-link" >Contact Form</Link>
-      </li>
-      <li className="nav-item dropdown">
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto  list">
+          <li className="nav-item active">
+            <Link to="/explore" className="nav-link">
+              Explore<span className="sr-only">(current)</span>
+            </Link>
+          </li>
+          <li className="nav-item active">
+            <Link to="/about" className="nav-link">
+              About Us<span className="sr-only">(current)</span>
+            </Link>
+          </li>
+          <li className="nav-item active">
+            <Link to="/contact" className="nav-link">
+              Contact Form
+            </Link>
+          </li>
+           {isAuthenticated && (
+            <li>
+              <img className="profile" src={user.picture} alt={user.name} />
+       
+            </li>
+          )} 
+
+          {isAuthenticated ? (
+           
+               
+              <button
+                type="button"
+                class="btn btn-primary btn-lg"
+                onClick={() =>
+                  logout({ logoutParams: { returnTo: window.location.origin } })
+                }
+              >
+                Log Out
+              </button>
+            
+          ) : (
+            <button
+              type="button"
+              class="btn btn-primary btn-lg"
+              onClick={() => loginWithRedirect()}
+            >
+              Log In
+            </button>
+          )}
+
+          {/* <li className="nav-item dropdown">
         <Link className="nav-link dropdown-toggle"  id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
          SignUP
         </Link>
@@ -32,14 +72,13 @@ function NavBar() {
           <div className="dropdown-divider"></div>
           <Link className="dropdown-item" >Something else here</Link>
         </div>
-      </li>
-      {/* <li className="nav-item">
+      </li> */}
+          {/* <li className="nav-item">
         <a className="nav-link disabled" >Disabled</a>
       </li> */}
-    </ul>
-   
-  </div>
-</nav>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,13 +5,20 @@ import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App.jsx";
 import "./index.css";
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Auth0Provider   domain="dev-igc1c6qf4zeug704.us.auth0.com"
+    clientId="RWPd9Cp6Ah1rjZQy9uu6i3HHUkc2nfw9"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}>
     <BrowserRouter>
       <ChakraProvider>
         <App />
       </ChakraProvider>
     </BrowserRouter>
+    </Auth0Provider>
   </React.StrictMode>
 );
